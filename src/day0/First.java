8d5c701099201e6d6b3420a52b4b860fcd6bff89
uajwau <60240513+uajwau@users.noreply.github.com>
2022-03-26 16:13:31 -0400

    added first file

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,8 +1,9 @@
 package day0;
 
 public class First {
-
-        //todo: work on this tomorrow
-        // add some cool code
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("working...");
+    }
 
 }
